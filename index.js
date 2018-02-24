1156a0f92343f95cf80004279641bfb0948bfa76
Charlotte Payne <cltpayne@gmail.com>
2018-02-24 17:24:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function nestedTarget() {
   return document.querySelector('#nested .target')
 }
 
+function deepestChild() {
+  return document.querySelectorAll('div#grand-node:last-child')
+}
+
 function increaseRankBy(n) {
   const total = document.querySelectorAll('.ranked-list')
   for (let i = 0; i < total.length; i++) {total.parseInt(i) (i + n)}
