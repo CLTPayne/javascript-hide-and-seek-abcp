cf55326c7094c3321d2397fc46684ce6b5cdd986
Charlotte Payne <cltpayne@gmail.com>
2018-02-24 17:48:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,11 @@ function increaseRankBy(n) {
     rankedList[i].innerHTML=(parseInt(rankedList[i].innerHTML) + n)}
 }
 
+
+function increaseRankBy(n){
+  var lis =
+  document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+   lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n);
+  }
+}
