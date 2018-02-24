c516ae5fb957f4ee14b10c64e848de35e49402f4
Charlotte Payne <cltpayne@gmail.com>
2018-02-24 16:49:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function getFirstSelector(selector) {
   return document.querySelector(selector)
 }
 
-function nestedTarget() {
+function nestedTarget(div) {
   document.querySelectorAll(div.target)
 }
