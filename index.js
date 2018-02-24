bbb9b1be68b2addb0dca5a79bd1af7c5a827114c
Charlotte Payne <cltpayne@gmail.com>
2018-02-24 17:36:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,26 @@ function deepestChild() {
 
 function increaseRankBy(n) {
   const rankedList = document.querySelectorAll('.ranked-list')
-  for (let i = 0; i < rankedList.length; i++) {(parseInt(i)) (i + n)}
+  const firstList = rankedLists[0]
+  const secondList = rankedLists[1]
+  let childred = firstList.children
+  let start = 1
+  for (let i = 0; l = children.length; i < l; i++) {(parseInt(children[i].innerHTML)) + n)}
 }
+
+const rankedLists = document.querySelectorAll('.ranked-list')
+      const firstList = rankedLists[0]
+      const secondList = rankedLists[1]
+
+      let children = firstList.children
+      let start = 1
+      for (let i = 0, l = children.length; i < l; i++) {
+        expect(parseInt(children[i].innerHTML)).toEqual(start + i + 3)
+      }
+
+      children = secondList.children
+      start = 12
+
+      for (let i = 0, l = children.length; i < l; i++) {
+        expect(parseInt(children[i].innerHTML)).toEqual(start - i + 3)
+      }
