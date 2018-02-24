6286305587fb0c17a13997fa166ad3505aa7e673
Charlotte Payne <cltpayne@gmail.com>
2018-02-24 16:54:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function getFirstSelector(selector) {
   return document.querySelector(selector)
 }
 
-function nestedTarget(div.target div#nested) {
-  document.querySelectorAll(div.target, div#nested)
+function nestedTarget(elmSel) {
+  document.querySelectorAll(elsSel)
 }
