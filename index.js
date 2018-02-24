9a71ceaf7f09491debc86cecf6d1c12f38a0ccd3
Charlotte Payne <cltpayne@gmail.com>
2018-02-24 17:03:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function getFirstSelector(selector) {
   return document.querySelector(selector)
 }
 
-function nestedTarget(.target) {
-  document.querySelectorAll()
+function nestedTarget() {
+  
 }
