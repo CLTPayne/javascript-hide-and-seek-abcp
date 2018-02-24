200dce537747ccf3278614c8913facc5a8784777
Charlotte Payne <cltpayne@gmail.com>
2018-02-24 17:49:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ function deepestChild() {
   return document.querySelector('#grand-node div div div div')
 }
 
-function increaseRankBy(n) {
-  var rankedList = document.getElementById('app').querySelectorAll('ul .ranked-list li');
-  for (let i = 0; i < rankedList.length; i++) {
-    rankedList[i].innerHTML=(parseInt(rankedList[i].innerHTML) + n)}
-}
 
 
 function increaseRankBy(n){
